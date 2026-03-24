0c428c83393558625f83662e1c7a706767a56378
alyaivaostavtevpokoe-crypto <alya.iva.ostavte.v.pokoe@gmail.com>
2026-03-24 20:51:24 +0300

    Initial Commit

---
+++ LineFlow/LineFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineFlowApp.swift
+//  LineFlow
+//
+//  Created by macbook Алиса on 24/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LineFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LineFlowTests/LineFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  LineFlowTests.swift
+//  LineFlowTests
+//
+//  Created by macbook Алиса on 24/3/26.
+//
+
+import Testing
+@testable import LineFlow
+
+struct LineFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
